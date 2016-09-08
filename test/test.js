2d3bc523f3c469900ca64540d742b263d4f31052
Patrick El-Hage <patrick.elhage@gmail.com>
2016-09-08 04:56:21 -0700

    Improved unit tests

--- test/test.js
+++ test/test.js
@@ -1,3 +1,4 @@
+// A lot of repetition, intentional for now
 import jsdom from 'jsdom';
 const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
 const win = doc.defaultView;
@@ -27,7 +28,7 @@ describe('<CategoriedInput />', () => {
     it('should render categories', () => {
       let wrapper = mount(<CategoriedInput categories={['Foo', 'Bar','Zoo']} />)
       expect(wrapper.childAt(1).children().length).to.equal(3)
-      expect(wrapper.childAt(1).hasClass('category-tag'))
+      expect(wrapper.childAt(1).hasClass('catreact-render__tag'))
       expect(wrapper.children().first().contains('Foo'))
       expect(wrapper.children().last().contains('Zoo'))
     })
@@ -36,22 +37,24 @@ describe('<CategoriedInput />', () => {
 
   describe('Adding categories', () => {
     it('should have a functioning onCategoryAdd handler', () => {
-      let categoryArr = ['Foo']
+      let categoryArr = ['Foo', 'Bar', 'Baz']
       let addedValue = ''
 
       let wrapper = mount(
         <CategoriedInput
           categories={categoryArr}
           onCategoryChange={e => { categoryArr = e }}
-          onCategoryRemove={e => { removedValue = e }}
+          onCategoryAdd={e => { addedValue = e }}
         />)
+
+      expect(wrapper.childAt(1).children().length).to.equal(3)
       let input = wrapper.find('input').get(0)
       wrapper.find('input').simulate('change', {target: {value: 'FooBar'}});
       wrapper.find('input').simulate("keyDown", {
         keyCode: 9,
         which: 9
       });
-      // expect(wrapper.childAt(1).children().length).to.equal(2)
+      // expect(wrapper.childAt(1).children().length).to.equal(4)
       expect(addedValue).to.equal('FooBar')
     })
 
@@ -62,6 +65,7 @@ describe('<CategoriedInput />', () => {
           categories={categoryArr}
           onCategoryChange={(e) => { categoryArr = e }}
         />)
+      expect(categoryArr.length).to.equal(2)
       let input = wrapper.find('input').get(0)
       wrapper.find('input').simulate('change', {target: {value: '    '}});
       wrapper.find('input').simulate("keyDown", {
@@ -78,6 +82,7 @@ describe('<CategoriedInput />', () => {
           categories={categoryArr}
           onCategoryChange={(e) => { categoryArr = e }}
         />)
+
       let input = wrapper.find('input').get(0)
       wrapper.find('input').simulate('change', {target: {value: 'FooBar'}});
       wrapper.find('input').simulate("keyDown", {
@@ -110,15 +115,17 @@ describe('<CategoriedInput />', () => {
   describe('Removing categories', () => {
 
     it('should delete categories', () => {
-      let categoryArr = ['Foo']
+      let categoryArr = ['Foo', 'Bar']
       let wrapper = mount(
         <CategoriedInput
           categories={categoryArr}
           onCategoryChange={(e) => { categoryArr = e }}
         />)
+      expect(wrapper.childAt(1).children().length).to.equal(2)
+      wrapper.find('.catreact-render__tag-close').first().simulate('click')
+      wrapper.setProps({categories: categoryArr })
       expect(wrapper.childAt(1).children().length).to.equal(1)
-      wrapper.find('.catreact-render__tag-close').simulate('click')
-      expect(categoryArr.length).to.equal(0)
+      expect(categoryArr.length).to.equal(1)
     })
 
     it('should have a functioning onCategoryRemove handler', () => {
