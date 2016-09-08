0bf1e776820b977c2cc92c584c49415993fe42d1
Patrick El-Hage <patrick.elhage@gmail.com>
2016-09-08 06:53:49 -0700

    Updates

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import CategoriedInput from '../src/CategoriedInput'
+// import './App.css'
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.onCategoriesUpdate = this.onCategoriesUpdate.bind(this)
+    this.state = { categories: [] }
+  }
+
+  onCategoriesUpdate(e) {
+    this.setState({ categories: e })
+  }
+
+  render() {
+    return (
+      <div>
+        <p style={{fontFamily:'Helvetica', paddingLeft: '10px'}}>Categories</p>
+        <CategoriedInput
+          placeholder="Enter a category"
+          onCategoriesUpdate={this.onCategoriesUpdate}
+          categories={this.state.categories} />
+      </div>
+    )
+  }
+}
